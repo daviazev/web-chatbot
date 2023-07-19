a4af89113dbc967755817b2c164af8c06067fad3
Davi Azevedo <daviazev@Davis-MacBook-Pro.local>
2023-07-19 07:36:25 -0700

    fix: fixes a bug in register user router

--- src/backend/model/RegisterODM.ts
+++ src/backend/model/RegisterODM.ts
@@ -1,13 +1,14 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import AbstractODM from "./AbstractODM";
 import IUser from "@/backend/interfaces/IUser.interface";
 
 class RegisterODM extends AbstractODM<IUser> {
   constructor() {
     const schema = new Schema<IUser>({
+      _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
       username: { type: String, required: true },
       password: { type: String, required: true },
-      createdAt: { type: Date, required: true },
+      createdAt: { type: Date, required: false },
     });
     super(schema, "Users");
   }
